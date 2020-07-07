7549c1e05b9c87d1d201c666e75c3a97ada6456a
nayacirdan <42298353+nayacirdan@users.noreply.github.com>
2020-07-07 03:55:42 +0300

    Updates

---
+++ precache-manifest.9c848e7ad9d3f34a9e5ca1ce90bc28d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52376465f49c6a311d779f9449b226c8",
+    "url": "/blynk/index.html"
+  },
+  {
+    "revision": "778186aa902134e6d290",
+    "url": "/blynk/static/css/main.c78b8604.chunk.css"
+  },
+  {
+    "revision": "c7d7ef4ed46c7cf1d658",
+    "url": "/blynk/static/js/2.6cae21e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blynk/static/js/2.6cae21e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778186aa902134e6d290",
+    "url": "/blynk/static/js/main.84fd8ddc.chunk.js"
+  },
+  {
+    "revision": "7a86ff200435da1b56a7",
+    "url": "/blynk/static/js/runtime-main.a2719b3f.js"
+  }
+]);
